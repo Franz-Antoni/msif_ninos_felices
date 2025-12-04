23ad63a56b2400cd2f6e66e3c734f40be2643450
Franz Antoni <antoni_franz@hotmail.com>
2025-12-04 10:08:14 -0500

    Configuracion y estructura del proyecto.

---
+++ src/test/java/pe/com/msif/MsifApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.msif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
